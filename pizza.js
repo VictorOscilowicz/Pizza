a44b6163e62a175744a92c09dee1ab2a5f7af317
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-11-17 10:53:52 -0600

    Add files via upload
    Began work on the new project.

---
+++ pizza.js
@@ -0,0 +1,35 @@
+class pizza{
+  //type constructor below
+  consructor (s,mT,vT){
+    this.size = s;
+    this.meatToppings = mT;
+    this.veggieToppings= vT;
+  }
+
+  //type instance functions below
+  sizeCost(){
+    if(this.size=="small"){
+      return 7.99;
+    }
+    else if(this.size=="medium"){
+      return 9.99;
+    }
+    else(this.size=="large"){
+      return 12.99;
+    }
+    else(this.size=="extra large"){
+      return 15.99;
+    }
+  }
+
+  //type class functions below
+  toppingCost(top){
+    if(this.top==meatToppings){
+      return this.
+    }
+    else if(this.top==veggieToppings){
+      return this.
+    }
+  }
+
+}
