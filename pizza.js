97c8facd3dc06a2d89d2b72c4b1143eac995ceaa
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-11-18 10:53:42 -0600

    Add files via upload
    Continued work

--- pizza.js
+++ pizza.js
@@ -1,4 +1,4 @@
-class pizza{
+xclass pizza{
   //type constructor below
   consructor (s,mT,vT){
     this.size = s;
@@ -9,27 +9,41 @@ class pizza{
   //type instance functions below
   sizeCost(){
     if(this.size=="small"){
-      return 7.99;
+      this.sizeCost=7.99;
+      return this.sizeCost;
     }
     else if(this.size=="medium"){
-      return 9.99;
+      this.sizeCost=9.99;
+      return this.sizeCost;
     }
     else(this.size=="large"){
-      return 12.99;
+      this.sizeCost=12.99;
+      return this.sizeCost;
     }
     else(this.size=="extra large"){
-      return 15.99;
+      this.sizeCost=15.99;
+      return this.sizeCost;
     }
   }
 
   //type class functions below
   toppingCost(top){
     if(this.top==meatToppings){
-      return this.
+      this.toppingCost=this.meatToppings.length*.99;
+      return this.toppingCost;
     }
     else if(this.top==veggieToppings){
-      return this.
+      this.toppingCost=this.veggieToppings.length*.50;
+      return this.toppingCost;
     }
   }
-
+  price(){
+    return this.sizeCost+this.toppingCost;
+  }
+  static promotionalDeal(p,pt){
+    this.pizza=p;
+    this.percentage=pt;
+    new price()=
+  }
+  addTopping()
 }
